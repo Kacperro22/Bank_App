442ccf71b30ec3528abdaf43739b6ab8bd869853
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-12-05 20:09:07 +0100

    wpłata

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -19,8 +20,26 @@ public class Main {
         User user1 = new User(name, lastName, pesel);
 
         System.out.println("Menu \n 1. Informacje o koncie \n 2. Wplata pieniedzy \n 3. Wyplata pieniedzy \n 4. Wyjscie ");
+        System.out.println("Wyierz opcję: ");
+        int option = scan.nextInt();
 
-
+        switch (option){
+            case 1:
+                user1.print();
+                break;
+            case 2:
+                System.out.println("Jaka kwote chcesz wplacic? ");
+                double nowaWplata = scan.nextDouble();
+                user1.deposit(nowaWplata);
+                System.out.println("Saldo konta wynosi: " + user1.getBalance());
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+        }
 
     }
 }
