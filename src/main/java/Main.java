d655d835d9030470593b9ebe900c16b1d9ebc63e
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-12-05 20:39:30 +0100

    wyjscie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Witaj w naszym banku!");
 
@@ -18,31 +18,35 @@ public class Main {
 
 
         User user1 = new User(name, lastName, pesel);
-
+        int option;
+        do {
         System.out.println("Menu \n 1. Informacje o koncie \n 2. Wplata pieniedzy \n 3. Wyplata pieniedzy \n 4. Wyjscie ");
         System.out.println("Wyierz opcję: ");
-        int option = scan.nextInt();
+        option = scan.nextInt();
 
-        switch (option){
-            case 1:
-                user1.print();
-                break;
-            case 2:
-                System.out.println("Jaka kwote chcesz wplacic? ");
-                double nowaWplata = scan.nextDouble();
-                user1.deposit(nowaWplata);
-                System.out.println("Saldo konta wynosi: " + user1.getBalance());
-                break;
-            case 3:
-                System.out.println("Jaka kwote chcesz wyplacic? ");
-                double nowaWyplata = scan.nextDouble();
-                user1.withdraw(nowaWyplata);
-                System.out.println("Saldo konta wynosi: " + user1.getBalance());
-                break;
-            case 4:
-                System.out.println("4");
-                break;
+            switch (option) {
+                case 1:
+                    user1.print();
+                    break;
+                case 2:
+                    System.out.println("Jaka kwote chcesz wplacic? ");
+                    double nowaWplata = scan.nextDouble();
+                    user1.deposit(nowaWplata);
+                    System.out.println("Saldo konta wynosi: " + user1.getBalance());
+                    break;
+                case 3:
+                    System.out.println("Jaka kwote chcesz wyplacic? ");
+                    double nowaWyplata = scan.nextDouble();
+                    user1.withdraw(nowaWyplata);
+                    System.out.println("Saldo konta wynosi: " + user1.getBalance());
+                    break;
+                case 4:
+                    System.out.println("Do zobaczenia! ");
+                    break;
+            }
+
         }
-
+        while (option!=4);
     }
+
 }
