400ec41a5eabd2de38e3312244bfe88688bdc363
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-12-05 18:24:48 +0100

    ok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Witaj w naszym banku!");
+
+        System.out.println("Wprowadź swoje imię: ");
+        String name = scan.nextLine();
+
+        System.out.println("Wprowadź swoje nazwisko: ");
+        String lastName = scan.nextLine();
+
+        System.out.println("Wprowadź swoje pesel: ");
+        int pesel = scan.nextInt();
+
+
+        System.out.println("Menu \n 1. Informacje o koncie \n 2. Wplata pieniedzy \n 3. Wyplata pieniedzy \n 4. Wyjscie ";
+        );
+
+
+
+
     }
 }
